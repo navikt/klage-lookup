72404277b70bd0dc7da26f3b54cdb6718b91fb9a
Andreas Jonsson <andreas.jonsson@nav.no>
2026-01-23 15:21:32 +0100

    Make Access serializable for Redis/Valkey.

--- src/main/kotlin/no/nav/klage/lookup/service/access/Access.kt
+++ src/main/kotlin/no/nav/klage/lookup/service/access/Access.kt
@@ -1,6 +1,12 @@
 package no.nav.klage.lookup.service.access
 
+import java.io.Serializable
+
 data class Access(
     val access: Boolean,
     val reason: String,
-)
+) : Serializable {
+    companion object {
+        private const val serialVersionUID: Long = 1L
+    }
+}
