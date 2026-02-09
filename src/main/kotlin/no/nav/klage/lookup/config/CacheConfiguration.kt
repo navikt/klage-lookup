f457964b78aadb577924d3b1ca8bdd8118084990
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-02-09 08:48:50 +0100

    Different serialization approach, lifted from nav-persondata-api.

--- src/main/kotlin/no/nav/klage/lookup/config/CacheConfiguration.kt
+++ src/main/kotlin/no/nav/klage/lookup/config/CacheConfiguration.kt
@@ -8,9 +8,10 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
-import tools.jackson.module.kotlin.jacksonObjectMapper
+import tools.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import java.time.Duration
 
 @EnableCaching
@@ -20,16 +21,33 @@ class CacheConfiguration {
     companion object {
         const val ACCESS_TO_PERSON = "accessToPerson"
         const val USERS_GROUPS = "usersGroups"
+
+        internal fun createRedisSerializer(): GenericJacksonJsonRedisSerializer {
+            val typeValidator = BasicPolymorphicTypeValidator.builder()
+                .allowIfSubType("no.nav")
+                .allowIfSubType("java.time")
+                .allowIfSubType("java.math")
+                .allowIfSubType("java.util.ArrayList")
+                .allowIfBaseType(Collection::class.java)
+                .allowIfBaseType(Map::class.java)
+                .build()
+
+            return GenericJacksonJsonRedisSerializer
+                .builder()
+                .enableDefaultTyping(typeValidator)
+                .build()
+        }
     }
 
     @Bean
     fun cacheManager(redisConnectionFactory: RedisConnectionFactory): RedisCacheManager {
         val defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+        val serializer = createRedisSerializer()
 
         val standardConfig = RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(10))
             .serializeKeysWith(fromSerializer(StringRedisSerializer()))
-            .serializeValuesWith(fromSerializer(GenericJacksonJsonRedisSerializer(jacksonObjectMapper())))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
 
         return RedisCacheManager.builder(redisConnectionFactory)
             .cacheDefaults(defaultConfig)
