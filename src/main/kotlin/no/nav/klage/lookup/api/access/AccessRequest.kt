a0692c27bef7ce925a7f17ec25c4f4437edda261
Andreas Jonsson <andreas.jonsson@nav.no>
2026-04-21 13:55:24 +0200

    Document removal.

--- src/main/kotlin/no/nav/klage/lookup/api/access/AccessRequest.kt
+++ src/main/kotlin/no/nav/klage/lookup/api/access/AccessRequest.kt
@@ -5,5 +5,6 @@ import no.nav.klage.lookup.api.common.Sak
 data class AccessRequest(
     val brukerId: String,
     val navIdent: String?,
+    @Deprecated("Sak is being removed. Clients need to call api once for every person in sak instead.")
     val sak: Sak?,
 )
